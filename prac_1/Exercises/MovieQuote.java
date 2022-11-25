5f9ed073205c1865328372855a93c7f5329ed294
thantzin23 <thantzin23523@gmail.com>
2022-11-25 12:02:46 +0800

    practical_1  exerice commit

---
+++ prac_1/Exercises/MovieQuote.java
@@ -0,0 +1,5 @@
+public class MovieQuote {
+    public static void main (String[] args){
+        System.out.println(" “Treasure the experience. Dreams fade away after you wake up.” – Hitoha Miyamizu");
+    }
+}
---
+++ prac_1/Exercises/MovieQuoteInfo.java
@@ -0,0 +1,5 @@
+public class MovieQuoteInfo {
+    public static void main (String[] args) {
+        System.out.println("The Quote is come from \n - Your Name - Anime Moive \n – Hitoha Miyamizu - Who is Mitsuha Miyamizu 's Grand ma \n 2016 November 3 ");
+    }
+}
