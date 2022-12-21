6302f4c34dc30a096209300925607bde95681207
thantzin23 <thantzin23523@gmail.com>
2022-12-21 16:43:17 +0800

    Practical_5 CodeInFigure_ch_8 commit

---
+++ prac_5/CodeInFigures_ch8/Employeess.java
@@ -0,0 +1,18 @@
+public class Employeess
+{
+   private int empNum;
+   private double empSal;
+   Employeess(int e, double s)
+   {
+      empNum = e;
+      empSal = s;
+   }
+   public int getEmpNum()
+   {
+      return empNum;
+   }
+   public double getSalary()
+   {
+      return empSal;
+   }
+}
